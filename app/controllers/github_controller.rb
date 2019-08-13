859fd081919d3b6fb8e2a8e9c88090fedbfd5574
Maysam Torabi <kherad@gmail.com>
2019-08-13 08:15:48 +0430

    showing not found message

--- app/controllers/github_controller.rb
+++ app/controllers/github_controller.rb
@@ -2,15 +2,16 @@ require 'open-uri'
 
 class GithubController < ApplicationController
   def search
-    if @query = params[:q]
+    @query = params[:q].to_s
+    if @query != ''
       @per_page = 20
       @page = params[:page] ? params[:page].to_i : 1
 
       @url = "https://api.github.com/search/repositories?q=#{@query}&per_page=#{@per_page}&page=#{@page}"
       Rails.logger.info "url = #{@url}"
       @response = JSON.parse open(@url).read
-      total = @response['total_count']
-      @pages = (total / @per_page).ceil
+      @total = @response['total_count'].to_i
+      @pages = (@total / @per_page).ceil
       @max_available_pages = [1000/@per_page, @pages].min
       @pagination_pages = (1..@max_available_pages).map { |i| i }.select{|i|
         # message: "Only the first 1000 search results are available",
